900bb6e82b2e5841cf8ded050dbc4b2cc0626824
engleilarodrigues <39334895+engleilarodrigues@users.noreply.github.com>
2018-05-31 18:52:30 -0300

    Add files via upload

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -27,22 +27,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	          <span class="navbar-toggler-icon"></span>
 	        </button>
 	        <div class="collapse navbar-collapse" id="navbarResponsive">
-	          <ul class="navbar-nav ml-auto">
-	            <li class="nav-item active">
-	              <a class="nav-link" href="#">Início
-	                <span class="sr-only">(current)</span>
-	              </a>
-	            </li>
-	            <li class="nav-item">
-	              <a class="nav-link" href="#">Sobre Nós</a>
-	            </li>
-	            <li class="nav-item">
-	              <a class="nav-link" href="#">Services</a>
-	            </li>
-	            <li class="nav-item">
-	              <a class="nav-link" href="#">Contact</a>
-	            </li>
-	          </ul>
+
 	        </div>
 	      </div>
 	    </nav>
@@ -51,11 +36,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	    <div class="container">
 
 	      <!-- Introduction Row -->
-	      <h1 class="my-4">Quem Somos
+	      <h1 class="my-4">Bem-Vindo!
 					<br>
 	        <small>Equipe de programadores em Tecnologias Web</small>
 	      </h1>
-	      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, explicabo dolores ipsam aliquam inventore corrupti eveniet quisquam quod totam laudantium repudiandae obcaecati ea consectetur debitis velit facere nisi expedita vel?</p>
+	      <p>Trabalho 03 da Disciplina de Tecnologias Web 2018.2 
+	      O objetivo do trabalho é desenvolver um site usando os conhecimentos adquiridos até o presente momento na disciplina. O site foi desenvolvido utilizando CodeIgniter e Bootstrap.</p>
+	      <br><br>
+	      <h4>O que é CodeIgniter?</h4>
+	      <p>O Codeigniter é um framework para desenvolvimento web baseado na arquitetura MVC. 
+	      O framework apresenta uma estrutura que lhe permite de forma rápida fazer uso de bibliotecas para ganhar tempo e aproveitar a reutilização de código. Algumas vantagens do CodeIgniter são:</p>
+	      <ul>
+	      	<li>Boa documentação;</li>
+	      	<li>Sem necessidade de instalação;</li>
+	      	<li>É muito leve em relação a outros frameworks;</li>
+	      	<li>Não precisa se preocupar tanto com pré-requisitos em servidores;</li>
+	      </ul>
+
+	      <h4>O que é Bootstrap?</h4>
+	      <p> Bootstrap é um framework web com código-fonte aberto para desenvolvimento de componentes de interface e front-end para sites e aplicações web usando HTML, CSS e JavaScript, baseado em modelos de design para a tipografia, melhorando a experiência do usuário em um site amigável e responsivo.
+	      O Bootstrap é um dos projetos mais bem avaliado no site GitHub, com mais de 111 600 estrelas e 51 500 forks.</p>
 
 	      <!-- Team Members Row -->
 	      <div class="row">
@@ -64,8 +64,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	        </div>
 	        <div class="col-lg-3 col-sm-6 text-center mb-4">
 						<a href="<?php echo base_url();?>index.php/mateus/sobre/">
-
-							<img class="rounded-circle img-fluid d-block mx-auto" src="<?= base_url("images/mateus.jpeg");?>" alt="">
+							<!-- Aqui são carregadas as imagens clicáveis. Copiem os links de acordo com as views de cada um de vocês-->
+							<img class="rounded-circle img-fluid d-block mx-auto" src="<?= base_url("images/mateus.jpeg");?>" style="width: 200px; height: 200px" alt="">
 						</a>
 						<h3>Mateus Arruda
 	            <small>Programador</small>
@@ -73,21 +73,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	          <p>Programador Python e Java atualmente passando fome por conta do desemprego.</p>
 	        </div>
 	        <div class="col-lg-3 col-sm-6 text-center mb-4">
-	          <img class="rounded-circle img-fluid d-block mx-auto" src="<?= base_url("images/leila.jpeg");?>" alt="">
-	          <h3>Leila Maria
-	            <small>Job Title</small>
+	          <img class="rounded-circle img-fluid d-block mx-auto" src="<?= base_url("images/leiila.jpeg");?>" style="width: 200px; height: 200px" alt="">
+	          <h3>Leila Rodrigues
+	            <small>Graduanda em Engenharia da Computação/small>
 	          </h3>
 	          <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
 	        </div>
 	        <div class="col-lg-3 col-sm-6 text-center mb-4">
-	          <img class="rounded-circle img-fluid d-block mx-auto" src="<?= base_url("images/paulo.jpeg");?>" alt="">
+	          <img class="rounded-circle img-fluid d-block mx-auto" src="<?= base_url("images/paulo.jpeg");?>" style="width: 200px; height: 200px" alt="">
 	          <h3>Paulo Victor
 	            <small>Desempregado</small>
 	          </h3>
 	          <p>E só.</p>
 	        </div>
 	        <div class="col-lg-3 col-sm-6 text-center mb-4">
-	          <img class="rounded-circle img-fluid d-block mx-auto"  src="http://placehold.it/200x200" alt="">
+	          <img class="rounded-circle img-fluid d-block mx-auto"  src="<?= base_url("images/lucas.jpeg");?>" style="width: 200px; height: 200px" alt="">
 	          <h3>Lucas Mota
 	            <small>Job Title</small>
 	          </h3>
