0b2ea44db746428f587f3571a1f3ddf9f9a714f5
PauloVictor87 <paulovictorfc87@gmail.com>
2018-05-31 19:59:18 -0300

    Create paulo.php

---
+++ application/controllers/paulo.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Mateus extends CI_Controller {
+  public function sobre()
+  {
+    //para cada controlador coloca 'image' com o caminho do arquivo da foto
+    //de cada membro
+    $data['image'] = base_url("images/paulo.jpeg");
+    $data['description'] = " oi sou o Paulo Victor, graduando em engenharia da
+     computação pela UFC campus Sobral, programador junior me especializando em desenvolvimento web, mobile, bem como na criação e gerenciamentos de moodles para instituições de ensino. ";
+    $this->load->view('header');
+    $this->load->view('body',$data);
+
+  }
+}
