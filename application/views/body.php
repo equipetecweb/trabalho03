5cd432975cc4ef79793dc5da07335a8aa5610094
engleilarodrigues <39334895+engleilarodrigues@users.noreply.github.com>
2018-05-31 20:24:01 -0300

    Add files via upload

--- application/views/body.php
+++ application/views/body.php
@@ -1,6 +1,10 @@
 <body>
   <br><br>
-<h1 class="mt-4">Post Title</h1>
+  <div class="container">
+    <h1 class="mt-4"> <?php echo $nome;?>
+    </h1>    <h3> <?php echo $cargo;?> </h3>
+  </div>
+
 <div class="col-sm">
 <img class="rounded-circle img-fluid d-block mx-auto" style="width: 400px; height: 400px" src="<?= $image;?>" alt="">
 </div>
@@ -8,3 +12,7 @@
 <div class="container">
   <?php echo $description;?>
 </div>
+
+<br><br>
+
+</body>
